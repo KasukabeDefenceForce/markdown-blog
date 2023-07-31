0711b43ae87b1eb00d97eea86ec4a95c1ac0e58b
AbhinavOhri <abhinavohri13@gmail.com>
2023-07-31 22:58:46 +0530

    Edit functionality

--- server.js
+++ server.js
@@ -2,29 +2,25 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const articleRouter = require("./routes/articles");
+const Article = require("./models/article");
+const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost/blog", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
-app.use("/articles", articleRouter);
-
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test articles",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-    {
-      title: "Test articles 2 ",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-  ];
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: "desc",
+  });
   res.render("articles/index", { articles: articles });
 });
+
+app.use("/articles", articleRouter);
 
 app.listen(5000);
